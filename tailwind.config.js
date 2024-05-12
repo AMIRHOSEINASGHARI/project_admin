eea912932e5315273f78db664ffef3a629c3b5b4
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-12 19:58:19 +0330

    tailwind base configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,48 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        lightOrange: "#ffedd5",
+        lightRose: "#ffe4e6",
+        lightPurple: "#ede9fe",
+        lightGray: "#f3f4f6",
+        lightSlate: "#cbd5e1",
+        lightBlue: "#dbeafe",
+        lightGreen: "#dcfce7",
+        darkOrange: "#c2410c",
+        darkRose: "#e11d48",
+        darkPurple: "#7c3aed",
+        darkGray: "#6b7280",
+        darkSlate: "#64748b",
+        darkBlue: "#2563eb",
+        darkGreen: "#22c55e",
+        dark1: "#161c24",
+        dark2: "#232b33",
+        // base color:
+        baseLight: "#d1fae5",
+        baseDark: "#10b981",
+      },
+      padding: {
+        box: "25px 20px",
+        btn: "5px 15px",
+      },
+      borderRadius: {
+        box: "30px",
+        btn: "8px",
+      },
+      fontSize: {
+        base: "16px",
+        h1: "30px",
+        h2: "25px",
+        h3: "20px",
+        h4: "17px",
+        p1: "14px",
+        p2: "12px",
+        p3: "10px",
+      },
+      gap: {
+        btn: "8px",
+        box: "14px",
       },
     },
   },
