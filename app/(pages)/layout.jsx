74bb1f21283fce7877d543cc980a9ec5da42c162
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-13 11:05:16 +0330

    Securing pages from role USER

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,7 +1,10 @@
 // next
 import { redirect } from "next/navigation";
+// actions
+import { getCurrentAdmin } from "@/actions/admin";
 // utils
 import { getServerSession } from "@/utils/session";
+// cmp
 import Sidebar from "@/components/shared/layout/Sidebar";
 import Navbar from "@/components/shared/layout/Navbar";
 
@@ -11,12 +14,15 @@ const PagesLayout = async ({ children }) => {
   if (!session) {
     redirect("/login");
   }
+
+  const data = await getCurrentAdmin();
+
   return (
     <div>
       <Navbar />
       <Sidebar />
       <div className="pages_spaces">
-        {session.roll === "USER" ? (
+        {data.currentAdmin.roll === "USER" ? (
           "not allowed"
         ) : (
           <div className="space-y-[20px]">
