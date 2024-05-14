dd96dac9bbf2c8d73c25426fcefd71450bf629d6
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-14 10:04:34 +0330

    refactoring getProducts server action

--- actions/product.js
+++ actions/product.js
@@ -75,7 +75,7 @@ export const createProduct = async (data) => {
   }
 };
 
-export const getProducts = async () => {
+export const getProducts = async (searchParams) => {
   try {
     await connectDB();
 
@@ -84,33 +84,82 @@ export const getProducts = async () => {
     // check session
     if (!session) {
       return {
-        message: "Un Authorized",
-        status: "failed",
-        code: 401,
+        message: MESSAGES.unAuthorized,
+        status: MESSAGES.failed,
+        code: STATUS_CODES.unAuthorized,
       };
     }
 
     // check user roll
     if (session.roll === "USER") {
       return {
-        message: "Access Denied!",
-        status: "failed",
-        code: 403,
+        message: MESSAGES.forbidden,
+        status: MESSAGES.failed,
+        code: STATUS_CODES.forbidden,
       };
     }
 
-    const products = await Product.find().lean();
+    const { page, search, has_selling_stock, has_discount, sort, category } =
+      searchParams;
+
+    let query = {};
+    let filters = {};
+
+    // search query filter
+    if (search) {
+      query = { $text: { $search: search } };
+    }
+    // product stock filter
+    if (has_selling_stock) {
+      filters.stock = { $gt: 0 };
+    }
+    // product discount filter
+    if (has_discount) {
+      has_discount == 1
+        ? (filters.discount = { $gt: 0 })
+        : (filters.discount = 0);
+    }
+    // product category filter
+    if (category) {
+      filters.category = category;
+    }
+
+    const pageNumber = page || 1;
+    const perPage = 8;
+    const totalProducts = await Product.countDocuments({
+      ...query,
+      ...filters,
+    });
+    const totalPages = Math.ceil(totalProducts / perPage);
+
+    const products = await Product.find({ ...filters, ...query })
+      .sort({
+        ...(sort == 1
+          ? { createdAt: -1 }
+          : sort == 2
+          ? { createdAt: 1 }
+          : sort == 3
+          ? { price: -1 }
+          : sort == 4
+          ? { price: 1 }
+          : sort == 5
+          ? { orders: -1 }
+          : {}),
+      })
+      .skip((pageNumber - 1) * perPage)
+      .limit(perPage)
+      .lean();
 
     return {
       products,
-      message: "success",
+      totalPages,
       status: "success",
       code: 200,
     };
   } catch (error) {
     console.log(error);
     return {
-      message: "Server Error!",
+      products: null,
       status: "failed",
       code: 500,
     };
