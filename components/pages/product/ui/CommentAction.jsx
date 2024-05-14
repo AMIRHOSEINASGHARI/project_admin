aeeb3e04c78e15987c7e4740386e7f2609e1aa4d
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-14 17:07:12 +0330

    using deleteComment server action

--- components/pages/product/ui/CommentAction.jsx
+++ components/pages/product/ui/CommentAction.jsx
@@ -7,6 +7,7 @@ import useServerAction from "@/hooks/callServerAction";
 // actions
 import {
   changeCommentAnswerStatus,
+  deleteComment,
   publishCommentStatus,
 } from "@/actions/comment";
 // utils
@@ -27,37 +28,107 @@ import { Modal, Popover } from "antd";
 
 const CommentAction = ({ _id, answer, status, published }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
   const [value, setValue] = useState(answer);
 
+  //   USE SERVER ACTION HOOK : START   //
   const { loading: publishLoading, fn: publish } = useServerAction(
     publishCommentStatus,
     {
       _id,
       action: "publish",
-    }
+    },
+    () => closePopover()
   );
-
   const { loading: draftLoading, fn: draft } = useServerAction(
     publishCommentStatus,
     {
       _id,
       action: "draft",
-    }
+    },
+    () => closePopover()
   );
-
+  const { loading: deleteLoading, fn: deletingComment } = useServerAction(
+    deleteComment,
+    { _id },
+    () => closePopover()
+  );
   const { loading: answerLoading, fn: changeAnswer } = useServerAction(
     changeCommentAnswerStatus,
     { _id, value },
-    () => handleClose()
+    () => closeModal()
   );
 
-  const showModal = () => {
+  //   MODAL FUNCTION's : START   //
+  const openModal = () => {
     setIsModalOpen(true);
   };
-  const handleClose = () => {
+  const closeModal = () => {
     setIsModalOpen(false);
   };
+  const modalTitle = (
+    <div className="flex items-center justify-between border-b pb-3">
+      <p className="text-p1 font-medium">#{shorterText(_id, 15)}</p>
+      <CustomButton
+        disabled={answerLoading}
+        icon={<CircleClose />}
+        classNames="hoverable"
+        onClick={closeModal}
+      />
+    </div>
+  );
+  const modalStyles = {
+    content: {
+      padding: "20px",
+    },
+    mask: {
+      backdropFilter: "blur(10px)",
+    },
+  };
+  const onSubmitModal = (e) => {
+    e.preventDefault();
 
+    if (value === answer) return;
+
+    changeAnswer();
+  };
+  const modalContent = (
+    <form className="space-y-5" onSubmit={onSubmitModal}>
+      <p
+        className={`py-1 px-2 rounded-btn text-p1 font-bold w-fit ${
+          status === "Not-Answered"
+            ? "bg-lightOrange text-darkOrange"
+            : "bg-lightGreen text-darkGreen"
+        }`}
+      >
+        {status}
+      </p>
+      <CustomTextarea
+        label="Your Answer"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <div className="w-full flex justify-end gap-2">
+        <CustomButton
+          disabled={answerLoading}
+          title="Cancel"
+          type="button"
+          onClick={closeModal}
+          classNames="border p-btn rounded-btn text-p1 hoverable"
+        />
+        <CustomButton
+          disabled={answerLoading}
+          title={answerLoading ? <Loader width={15} height={15} /> : "Submit"}
+          type="submit"
+          classNames={`p-btn rounded-btn text-p1 ${
+            answerLoading ? "bg-lightGray" : "bg-darkBlue text-white"
+          }`}
+        />
+      </div>
+    </form>
+  );
+
+  //   POPOVER FUNCTION's : START   //
   const popoverContent = (
     <div className="p-1 flex flex-col gap-1 w-[150px]">
       <CustomButton
@@ -100,11 +171,11 @@ const CommentAction = ({ _id, answer, status, published }) => {
       />
       <hr />
       <CustomButton
-        // onClick={() => draft()}
-        // disabled={!published || draftLoading}
+        onClick={() => deletingComment()}
+        disabled={deleteLoading || publishLoading || draftLoading}
         classNames="flex justify-center w-full"
         title={
-          draftLoading ? (
+          deleteLoading ? (
             <Loader width={15} height={15} className="py-1" />
           ) : (
             <div
@@ -118,76 +189,17 @@ const CommentAction = ({ _id, answer, status, published }) => {
       />
     </div>
   );
-
-  const modalTitle = (
-    <div className="flex items-center justify-between border-b pb-3">
-      <p className="text-p1 font-medium">#{shorterText(_id, 15)}</p>
-      <CustomButton
-        disabled={answerLoading}
-        icon={<CircleClose />}
-        classNames="hoverable"
-        onClick={handleClose}
-      />
-    </div>
-  );
-
-  const modalStyles = {
-    content: {
-      padding: "20px",
-    },
-    mask: {
-      backdropFilter: "blur(10px)",
-    },
+  const closePopover = () => {
+    setIsPopoverOpen(false);
   };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (value === answer) return;
-
-    changeAnswer();
+  const onOpenChange = (newOpen) => {
+    setIsPopoverOpen(newOpen);
   };
-
-  const modalContent = (
-    <form className="space-y-5" onSubmit={onSubmit}>
-      <p
-        className={`py-1 px-2 rounded-btn text-p1 font-bold w-fit ${
-          status === "Not-Answered"
-            ? "bg-lightOrange text-darkOrange"
-            : "bg-lightGreen text-darkGreen"
-        }`}
-      >
-        {status}
-      </p>
-      <CustomTextarea
-        label="Your Answer"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <div className="w-full flex justify-end gap-2">
-        <CustomButton
-          disabled={answerLoading}
-          title="Cancel"
-          type="button"
-          onClick={handleClose}
-          classNames="border p-btn rounded-btn text-p1 hoverable"
-        />
-        <CustomButton
-          disabled={answerLoading}
-          title={answerLoading ? <Loader width={15} height={15} /> : "Submit"}
-          type="submit"
-          classNames={`p-btn rounded-btn text-p1 ${
-            answerLoading ? "bg-lightGray" : "bg-darkBlue text-white"
-          }`}
-        />
-      </div>
-    </form>
-  );
 
   return (
     <div className="flex items-center gap-1">
       <CustomButton
-        onClick={showModal}
+        onClick={openModal}
         title={<Edit size={18} />}
         classNames="hoverable p-2 rounded-btn"
       />
@@ -195,13 +207,15 @@ const CommentAction = ({ _id, answer, status, published }) => {
         title={modalTitle}
         closeIcon={false}
         open={isModalOpen}
-        onCancel={handleClose}
+        onCancel={closeModal}
         footer={false}
         styles={modalStyles}
       >
         {modalContent}
       </Modal>
       <Popover
+        open={isPopoverOpen}
+        onOpenChange={onOpenChange}
         overlayInnerStyle={{
           padding: "0",
         }}
