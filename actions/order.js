a8e7e0292bab47dda24199d84310dc4fa1d93b59
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-16 15:37:30 +0330

    updateOrderStatus server action

--- actions/order.js
+++ actions/order.js
@@ -63,6 +63,26 @@ export const getOrder = async (id) => {
   try {
     await connectDB();
 
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
     const order = await Order.findById(id)
       .populate({
         path: "userId",
@@ -78,6 +98,54 @@ export const getOrder = async (id) => {
     return {
       order,
       message: "success",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
+
+export const updateOrderStatus = async (data) => {
+  try {
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
+    const { id, action } = data;
+
+    const order = await Order.findById(id);
+
+    if (action === "Completed") {
+      order.status = "Completed";
+    } else {
+      order.status = "Pending";
+    }
+    await order.save();
+
+    return {
+      message: "Process finished!",
       status: "success",
       code: 200,
     };
