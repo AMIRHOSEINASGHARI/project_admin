76da064d2d6e01159a2d16f1b3d7782dc0691709
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-16 15:32:40 +0330

    getOrders server action

--- actions/order.js
+++ actions/order.js
@@ -1,1 +1,39 @@
 "use server";
+
+// utils
+import connectDB from "@/utils/connectDB";
+// models
+import { Order } from "@/utils/models/order";
+import { Product } from "@/utils/models/product";
+import { User } from "@/utils/models/user";
+
+export const getOrders = async () => {
+  try {
+    await connectDB();
+
+    const orders = await Order.find()
+      .populate({
+        path: "userId",
+        model: User,
+      })
+      .populate({
+        path: "items.productId",
+        model: Product,
+      })
+      .lean();
+
+    return {
+      orders,
+      message: "success",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
