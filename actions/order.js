d3024adfddbb7e9fcf37bba9ac8eda89344af3f8
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-16 15:33:22 +0330

    ...

--- actions/order.js
+++ actions/order.js
@@ -2,6 +2,7 @@
 
 // utils
 import connectDB from "@/utils/connectDB";
+import { getServerSession } from "@/utils/session";
 // models
 import { Order } from "@/utils/models/order";
 import { Product } from "@/utils/models/product";
@@ -10,6 +11,26 @@ import { User } from "@/utils/models/user";
 export const getOrders = async () => {
   try {
     await connectDB();
+
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
 
     const orders = await Order.find()
       .populate({
