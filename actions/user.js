aa76abd88657d97b8bad1d5c028bd7466fc04519
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-19 07:55:12 +0330

    getUser server action

--- actions/user.js
+++ actions/user.js
@@ -47,3 +47,45 @@ export const getUsers = async () => {
     };
   }
 };
+
+export const getUser = async (id) => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
+    const user = await User.findById(id).select("-password").lean();
+
+    return {
+      user,
+      message: "success",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
