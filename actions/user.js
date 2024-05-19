8888fd3458f5642c4173bebd9b4536c6bf4dc688
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-19 07:54:07 +0330

    getUsers server action

--- actions/user.js
+++ actions/user.js
@@ -1,1 +1,49 @@
 "use server";
+
+// utils
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "@/utils/session";
+// models
+import { User } from "@/utils/models/user";
+
+export const getUsers = async () => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
+    const users = await User.find().select("-password").lean();
+
+    return {
+      users,
+      message: "success",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
