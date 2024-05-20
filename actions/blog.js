87a9588b619ecf245da7076bfae3ef2fdd97d881
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-20 11:00:25 +0330

    createBlog server action

--- actions/blog.js
+++ actions/blog.js
@@ -1,1 +1,56 @@
 "use server";
+
+// utils
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "@/utils/session";
+// models
+import { Blog } from "@/utils/models/blog";
+
+export const createBlog = async (data) => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
+    const { title, description, image, keywords, published } = data;
+
+    const newBlog = await Blog.create({
+      title,
+      description,
+      image,
+      keywords,
+      published,
+    });
+
+    return {
+      message: "Blog Created",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
