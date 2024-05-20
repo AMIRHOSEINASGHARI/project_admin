19d46535538393f165b76d47e7874acbd1cac1ac
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-20 19:20:07 +0330

    updateBlogStatus server action

--- actions/blog.js
+++ actions/blog.js
@@ -1,5 +1,7 @@
 "use server";
 
+// next
+import { revalidatePath } from "next/cache";
 // utils
 import connectDB from "@/utils/connectDB";
 import { getServerSession } from "@/utils/session";
@@ -126,6 +128,59 @@ export const getBlog = async (_id) => {
     return {
       blog,
       message: "success",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
+
+export const updateBlogStatus = async (data) => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    // check session
+    if (!session) {
+      return {
+        message: "Un Authorized",
+        status: "failed",
+        code: 401,
+      };
+    }
+
+    // check user roll
+    if (session.roll === "USER") {
+      return {
+        message: "Access Denied!",
+        status: "failed",
+        code: 403,
+      };
+    }
+
+    const { id, action } = data;
+
+    const blog = await Blog.findById(id);
+
+    if (action === "publish") {
+      blog.published = true;
+    } else if (action === "draft") {
+      blog.published = false;
+    }
+
+    await blog.save();
+
+    revalidatePath("/blogs");
+
+    return {
+      message: "Blog Updated!",
       status: "success",
       code: 200,
     };
