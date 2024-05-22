3a9b05c5634f3f2a45c91383ec89c344168136ea
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-22 17:57:57 +0330

    task model

---
+++ utils/models/task.js
@@ -0,0 +1,21 @@
+import { Schema, model, models } from "mongoose";
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, default: "todo" },
+  createdBy: { type: Schema.Types.ObjectId, ref: "Admin" },
+  assignees: [{ type: Schema.Types.ObjectId, ref: "Admin" }],
+  dueDate: { type: Date, required: true, default: () => Date.now() },
+  lables: { type: [String], default: [] },
+  subTasks: [
+    {
+      title: String,
+      status: String,
+    },
+  ],
+});
+
+const Task = models?.Task || model("Task", taskSchema);
+
+export default Task;
