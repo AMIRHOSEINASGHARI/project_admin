dd8fb37b4280bca4048feaf8c1db0c8e2a73d9ac
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-23 15:47:13 +0330

    using updateTaskStatus and deleteTask server action

--- components/pages/tasks/ui/TaskActions.jsx
+++ components/pages/tasks/ui/TaskActions.jsx
@@ -2,50 +2,105 @@
 
 // react
 import { useState } from "react";
+// actions
+import { deleteTask, updateTaskStatus } from "@/actions/task";
+// hooks
+import useServerAction from "@/hooks/callServerAction";
+// cmp
 import CustomButton from "@/components/shared/CustomButton";
+import Loader from "@/components/shared/Loader";
 import { CircleCheck, MenuDots, Trash } from "@/components/icons/Icons";
 import { Popover } from "antd";
 
 const TaskActions = ({ id, currentStatus }) => {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-  const popoverContent = (
-    <div className="popContainer w-[200px]">
-      <CustomButton
-        classNames={`popButton ${
-          currentStatus === "Todo" ? "text-darkBlue" : "hoverable"
-        }`}
-        title="Todo"
-        icon={currentStatus === "Todo" && <CircleCheck size={17} />}
-      />
-      <CustomButton
-        classNames={`popButton ${
-          currentStatus === "Progress" ? "text-darkOrange" : "hoverable"
-        }`}
-        title="Progress"
-        icon={currentStatus === "Progress" && <CircleCheck size={17} />}
-      />
-      <CustomButton
-        classNames={`popButton ${
-          currentStatus === "Done" ? "text-darkGreen" : "hoverable"
-        }`}
-        title="Done"
-        icon={currentStatus === "Done" && <CircleCheck size={17} />}
-      />
-      <hr />
-      <CustomButton
-        classNames="popButton hoverable"
-        title="Delete"
-        icon={<Trash size={17} />}
-      />
-    </div>
-  );
   const closePopover = () => {
     setIsPopoverOpen(false);
   };
   const onOpenChange = (newOpen) => {
     setIsPopoverOpen(newOpen);
   };
+
+  //   Pop Actions
+  const { loading: todoLoading, fn: todoFN } = useServerAction(
+    updateTaskStatus,
+    {
+      id,
+      status: "Todo",
+    },
+    () => closePopover()
+  );
+  const { loading: progressLoading, fn: progressFN } = useServerAction(
+    updateTaskStatus,
+    {
+      id,
+      status: "Progress",
+    },
+    () => closePopover()
+  );
+  const { loading: doneLoading, fn: doneFN } = useServerAction(
+    updateTaskStatus,
+    {
+      id,
+      status: "Done",
+    },
+    () => closePopover()
+  );
+  const { loading: deleteLoading, fn: deleteFN } = useServerAction(
+    deleteTask,
+    id,
+    () => closePopover()
+  );
+
+  //   Pop Content
+  const popoverContent = (
+    <div className="popContainer w-[200px] min-h-[150px] flex flex-col justify-center items-center">
+      {todoLoading || progressLoading || doneLoading || deleteLoading ? (
+        <Loader height={20} width={20} />
+      ) : (
+        <>
+          <CustomButton
+            classNames={`popButton ${
+              currentStatus === "Todo" ? "text-darkBlue" : "hoverable"
+            }`}
+            title="Todo"
+            icon={currentStatus === "Todo" && <CircleCheck size={17} />}
+            onClick={() => todoFN()}
+            disabled={todoLoading || progressLoading || doneLoading}
+          />
+          <CustomButton
+            classNames={`popButton ${
+              currentStatus === "Progress" ? "text-darkOrange" : "hoverable"
+            }`}
+            title="Progress"
+            icon={currentStatus === "Progress" && <CircleCheck size={17} />}
+            onClick={() => progressFN()}
+            disabled={todoLoading || progressLoading || doneLoading}
+          />
+          <CustomButton
+            classNames={`popButton ${
+              currentStatus === "Done" ? "text-darkGreen" : "hoverable"
+            }`}
+            title="Done"
+            icon={currentStatus === "Done" && <CircleCheck size={17} />}
+            onClick={() => doneFN()}
+            disabled={todoLoading || progressLoading || doneLoading}
+          />
+          <div className="bg-gray-200 w-full h-[1px]" />
+          <CustomButton
+            classNames="popButton hoverable"
+            title="Delete"
+            icon={<Trash size={17} />}
+            onClick={() => deleteFN()}
+            disabled={
+              todoLoading || progressLoading || doneLoading || deleteLoading
+            }
+          />
+        </>
+      )}
+    </div>
+  );
 
   return (
     <Popover
