2fbe33a64a28741f344fb2c6c39173dc14c8c3d8
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-05-23 14:10:10 +0330

    getTasks server action

--- actions/task.js
+++ actions/task.js
@@ -5,6 +5,7 @@ import connectDB from "@/utils/connectDB";
 import { getServerSession } from "@/utils/session";
 // models
 import Task from "@/utils/models/task";
+import Admin from "@/utils/models/admin";
 
 export const createTask = async (data) => {
   try {
@@ -24,6 +25,34 @@ export const createTask = async (data) => {
 
     return {
       message: "Task Created!",
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Server Error!",
+      status: "failed",
+      code: 500,
+    };
+  }
+};
+
+export const getTasks = async () => {
+  try {
+    await connectDB();
+
+    const tasks = await Task.find()
+      .populate({
+        path: "createdBy",
+        model: Admin,
+        select: "username name avatar roll",
+      })
+      .lean();
+
+    return {
+      tasks,
+      message: "success",
       status: "success",
       code: 200,
     };
