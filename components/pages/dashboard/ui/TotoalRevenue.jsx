99d168c31ba01662bf69c22c88b09ad393f8df63
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-06-16 22:32:01 +0330

    TotoalRevenue cmp

--- components/pages/dashboard/ui/TotoalRevenue.jsx
+++ components/pages/dashboard/ui/TotoalRevenue.jsx
@@ -1,24 +1,88 @@
 "use client";
 
-// constants
-import { totalRevenueChartOptions, totalRevenueChartSeries } from "@/constants";
-// cmp
-import ReactApexChart from "react-apexcharts";
+import { AreaChart, Card, Title } from "@tremor/react";
 
-const TotoalRevenue = () => {
-  return (
-    <div className="w-full box bg-white flex flex-col flex-1">
-      <h1 className="h3 mb-3">Totoal Revenue</h1>
-      <div className="w-full h-full flex items-center justify-center">
-        <ReactApexChart
-          height={400}
-          options={totalRevenueChartOptions}
-          series={totalRevenueChartSeries}
-          type="bar"
-        />
-      </div>
-    </div>
-  );
+const chartdata = [
+  {
+    date: "Jan 22",
+    Phone: 2890,
+    Laptop: 2338,
+  },
+  {
+    date: "Feb 22",
+    Phone: 2756,
+    Laptop: 2103,
+  },
+  {
+    date: "Mar 22",
+    Phone: 3322,
+    Laptop: 2194,
+  },
+  {
+    date: "Apr 22",
+    Phone: 3470,
+    Laptop: 2108,
+  },
+  {
+    date: "May 22",
+    Phone: 3475,
+    Laptop: 1812,
+  },
+  {
+    date: "Jun 22",
+    Phone: 3129,
+    Laptop: 1726,
+  },
+  {
+    date: "Jul 22",
+    Phone: 3490,
+    Laptop: 1982,
+  },
+  {
+    date: "Aug 22",
+    Phone: 2903,
+    Laptop: 2012,
+  },
+  {
+    date: "Sep 22",
+    Phone: 2643,
+    Laptop: 2342,
+  },
+  {
+    date: "Oct 22",
+    Phone: 2837,
+    Laptop: 2473,
+  },
+  {
+    date: "Nov 22",
+    Phone: 2954,
+    Laptop: 3848,
+  },
+  {
+    date: "Dec 22",
+    Phone: 3239,
+    Laptop: 3736,
+  },
+];
+
+const valueFormatter = function (number) {
+  return "$ " + new Intl.NumberFormat("us").format(number).toString();
 };
 
-export default TotoalRevenue;
+export default function TotoalRevenue() {
+  return (
+    <div className="box border">
+      <h1 className="h1">Total Revenue</h1>
+      <AreaChart
+        className="mt-4 h-72"
+        data={chartdata}
+        index="date"
+        yAxisWidth={65}
+        categories={["Phone", "Laptop"]}
+        colors={["indigo", "cyan"]}
+        valueFormatter={valueFormatter}
+        yAxisLabel="Revenue (USD)"
+      />
+    </div>
+  );
+}
