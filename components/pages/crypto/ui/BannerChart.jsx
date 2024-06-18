cddc66fb453161dfa9cd161dd907153369ec5cac
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-06-18 08:53:47 +0330

    ...

--- components/pages/crypto/ui/BannerChart.jsx
+++ components/pages/crypto/ui/BannerChart.jsx
@@ -9,53 +9,54 @@ import Loader from "@/components/shared/Loader";
 import { SparkAreaChart } from "@tremor/react";
 
 const BannerChart = ({ coinId }) => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["coins", coinId],
-    queryFn: fetchCoin,
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    cacheTime: 365 * 24 * 60 * 60,
-    staleTime: 365 * 24 * 60 * 60,
-  });
+  try {
+    const { data, isLoading, isError, error } = useQuery({
+      queryKey: ["coins", coinId],
+      queryFn: fetchCoin,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      cacheTime: 365 * 24 * 60 * 60,
+      staleTime: 365 * 24 * 60 * 60,
+    });
 
-  if (isLoading) {
-    return (
-      <div className="w-full flex justify-center my-[31px]">
-        <Loader width={20} height={20} />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className="my-2">
-        <p className="text-center">Error!</p>
-      </div>
-    );
-  }
-
-  let chartData2 = [];
-
-  for (let d of data.prices.entries()) {
-    if (d[0] % 20 === 0) {
-      chartData2.push({
-        day: d[1][0],
-        Price: d[1][1],
-      });
+    if (isLoading) {
+      return (
+        <div className="w-full flex justify-center my-[31px]">
+          <Loader width={20} height={20} />
+        </div>
+      );
     }
-  }
 
-  return (
-    <SparkAreaChart
-      data={chartData2}
-      categories={["Price"]}
-      index={"day"}
-      colors={["blue"]}
-      className="h-[80px] w-full"
-      showAnimation={true}
-      animationDuration={1300}
-    />
-  );
+    if (isError) {
+      throw new Error(error.message);
+    }
+
+    let chartData2 = [];
+
+    for (let d of data.prices.entries()) {
+      if (d[0] % 20 === 0) {
+        chartData2.push({
+          day: d[1][0],
+          Price: d[1][1],
+        });
+      }
+    }
+
+    return (
+      <SparkAreaChart
+        data={chartData2}
+        categories={["Price"]}
+        index={"day"}
+        colors={["blue"]}
+        className="h-[80px] w-full"
+        showAnimation={true}
+        animationDuration={1300}
+      />
+    );
+  } catch (error) {
+    console.log(error);
+    return <p>{JSON.stringify(error.message)}</p>;
+  }
 };
 
 export default BannerChart;
