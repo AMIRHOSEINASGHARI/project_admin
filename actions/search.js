ff079ba18823addf6193247dc92327fa25e505ec
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-06-24 14:40:02 +0330

    searchDashboard server action

---
+++ actions/search.js
@@ -0,0 +1,45 @@
+// utils
+import connectDB from "@/utils/connectDB";
+// models
+import Admin from "@/utils/models/admin";
+import { Blog } from "@/utils/models/blog";
+import { Comment } from "@/utils/models/comment";
+import { Product } from "@/utils/models/product";
+import { User } from "@/utils/models/user";
+import Task from "@/utils/models/task";
+
+export const searchDashboard = async (searchQuery) => {
+  try {
+    await connectDB();
+
+    let query = { $text: { $search: searchQuery } };
+
+    const products = await Product.find(query).lean();
+    const blogs = await Blog.find(query).lean();
+    const tasks = await Task.find(query).lean();
+    const users = await User.find(query).lean();
+    const admins = await Admin.find(query).lean();
+    const comments = await Comment.find(query).lean();
+
+    return {
+      result: {
+        products,
+        blogs,
+        tasks,
+        users,
+        admins,
+        comments,
+      },
+      status: "success",
+      code: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      products: null,
+      status: "failed",
+      code: 500,
+      error,
+    };
+  }
+};
